21fb768f9017dc1421eea8b9903599287fe33832
Richard Thomas <richard.thomas@uq.edu.au>
2026-02-20 16:48:41 +1000

    Fixed return values.

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -20,7 +20,7 @@ def get_todos():
 
 @api.route('/todos/<int:id>', methods=['GET'])
 def get_todo(id):
-    return jsonify([{
+    return jsonify({
         "id": id,
         "title": "Watch CSSE6400 Lecture",
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
@@ -28,11 +28,11 @@ def get_todo(id):
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    }])
+    })
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
-    return jsonify([{
+    return jsonify({
         "id": 1,
         "title": "Watch CSSE6400 Lecture",
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
@@ -40,11 +40,11 @@ def create_todo():
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    }]), 201
+    }), 201
 
 @api.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
-    return jsonify([{
+    return jsonify({
         "id": id,
         "title": "Watch CSSE6400 Lecture",
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
@@ -52,11 +52,11 @@ def update_todo(id):
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    }])
+    })
 
 @api.route('/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
-    return jsonify([{
+    return jsonify({
         "id": id,
         "title": "Watch CSSE6400 Lecture",
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
@@ -64,4 +64,4 @@ def delete_todo(id):
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    }])
+    })
